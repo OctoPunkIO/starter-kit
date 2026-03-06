90fee8ddebe79f85f82a14cfd82d05597bb76172
MsOctoPunk <msoctopunk@example.com>
2026-03-06 10:59:43 -0500

    Commit A: Add test file with 10 lines

---
+++ test-comment-visibility.js
@@ -0,0 +1,10 @@
+// Line 1: Header
+// Line 2: Description
+// Line 3: Info
+// Line 4: Context
+// Line 5: THE COMMENTED LINE
+// Line 6: More context
+// Line 7: Additional info
+// Line 8: Footer start
+// Line 9: Footer middle
+// Line 10: Footer end
