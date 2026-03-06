fcdcb8f935e1314fe4fb86122204bb7db4b823fd
MsOctoPunk <msoctopunk@test.com>
2026-03-06 09:24:19 -0500

    Commit 1: Add test file with important code on line 8

---
+++ test-file.js
@@ -0,0 +1,12 @@
+// Line 1: Header comment
+// Line 2: More comments
+// Line 3: Even more comments
+
+// Line 5: Function declaration
+function hello() {
+  // Line 7: Inside function
+  console.log("Hello World"); // Line 8: THE IMPORTANT LINE
+  // Line 9: After log
+}
+
+// Line 12: Footer
