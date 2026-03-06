5757c49943ff82e21a103156fc2a2e6ce07377df
MsOctoPunk <msoctopunk@test.com>
2026-03-06 09:25:28 -0500

    Commit 2: Add 5 lines at top, shifting everything down

--- test-file.js
+++ test-file.js
@@ -1,12 +1,18 @@
-// Line 1: Header comment
-// Line 2: More comments
-// Line 3: Even more comments
+// NEW Line 1: Added in commit 2
+// NEW Line 2: More new content
+// NEW Line 3: Even more new content
+// NEW Line 4: This shifts everything down by 5 lines
+// NEW Line 5: Original line 1 is now line 6
 
-// Line 5: Function declaration
+// Line 6 (was 1): Header comment
+// Line 7 (was 2): More comments
+// Line 8 (was 3): Even more comments
+
+// Line 10 (was 5): Function declaration
 function hello() {
-  // Line 7: Inside function
-  console.log("Hello World"); // Line 8: THE IMPORTANT LINE
-  // Line 9: After log
+  // Line 12 (was 7): Inside function
+  console.log("Hello World"); // Line 13 (was 8): THE IMPORTANT LINE - now shifted!
+  // Line 14 (was 9): After log
 }
 
-// Line 12: Footer
+// Line 17 (was 12): Footer
