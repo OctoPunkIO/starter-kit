3f771fbfb831f76be8eb01bf71b38172f4816774
MsOctoPunk <support@octopunk.io>
2026-03-20 21:28:53 -0400

    Add math functions

---
+++ math.py
@@ -0,0 +1,14 @@
+def hello():
+    print("Hello, World!")
+
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    return a / b
